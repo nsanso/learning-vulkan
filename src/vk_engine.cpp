709fdaa127d5636b02329a2b2dcd03d4805ba5ff
nsanso <nsansoni97@gmail.com>
2023-07-03 02:29:16 +0200

    ch1 - Instance

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,29 +1,26 @@
 #include "vk_engine.h"
 
-#include <SDL.h>
-#include <SDL_vulkan.h>
-
 #include "vk_init.h"
 #include "vk_types.h"
+#include <SDL.h>
+#include <SDL_vulkan.h>
+#include <VkBootstrap.h>
+#include <iostream>
+
+// public
 
 void VulkanEngine::init() {
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_WindowFlags window_flags = (SDL_WINDOW_VULKAN);
 
+  SDL_WindowFlags window_flags = (SDL_WINDOW_VULKAN);
   this->window = SDL_CreateWindow(
       "Learning Vulkan", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-      this->windowExtent.width, this->windowExtent.height, window_flags);
+      this->window_extent.width, this->window_extent.height, window_flags);
 
-  this->isInitialized = true;
+  this->init_vulkan();
+
+  this->is_initialized = true;
 }
-
-void VulkanEngine::cleanup() {
-  if (this->isInitialized) {
-    SDL_DestroyWindow(this->window);
-  }
-}
-
-void VulkanEngine::draw() {}
 
 void VulkanEngine::run() {
   SDL_Event event;
@@ -31,12 +28,49 @@ void VulkanEngine::run() {
   while (true) {
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
-        goto QUIT;
+        goto label_VulkanEngine_run_QUIT;
       }
     }
     draw();
   }
 
-QUIT:
+label_VulkanEngine_run_QUIT:
   return;
+}
+
+void VulkanEngine::draw() {}
+
+void VulkanEngine::cleanup() {
+  if (this->is_initialized) {
+    SDL_DestroyWindow(this->window);
+  }
+}
+
+// private
+
+void VulkanEngine::init_vulkan() {
+  vkb::InstanceBuilder builder;
+
+  auto maybe_instance = builder.set_app_name("Learning Vulkan")
+                            .request_validation_layers()
+                            // .require_api_version(VK_MAKE_VERSION(1, 3, 5))
+                            .use_default_debug_messenger()
+                            .build();
+
+  if (!maybe_instance.has_value()) {
+    std::cout << "Failed to build vulkan instance: "
+              << maybe_instance.full_error().vk_result << std::endl;
+    abort();
+  }
+
+  vkb::Instance instance = maybe_instance.value();
+  this->instance = instance.instance;
+  this->debug_messenger = instance.debug_messenger;
+}
+
+void vk_check(VkResult err) {
+  if (err) {
+    std::cout << "Detected Vulkan error: " << err << std::endl;
+    abort();
+  }
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -2,16 +2,23 @@
 
 #include "vk_types.h"
 
-struct VulkanEngine {
-  bool isInitialized{false};
-  size_t frameNumber{0};
-
-  VkExtent2D windowExtent{1280, 720};
-
-  struct SDL_Window *window{nullptr};
-
+class VulkanEngine {
+public:
   void init();
   void cleanup();
   void draw();
   void run();
+
+private:
+  bool is_initialized{false};
+  size_t frame_number{0};
+  VkExtent2D window_extent{1280, 720};
+  struct SDL_Window *window{nullptr};
+  VkInstance instance;
+  VkDebugUtilsMessengerEXT debug_messenger;
+  VkPhysicalDevice chosen_gpu;
+  VkDevice device;
+  VkSurfaceKHR surface;
+
+  void init_vulkan();
 };
