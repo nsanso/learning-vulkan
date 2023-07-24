281f3e314ef9b081828424d380d48c953c21f67d
nsanso <nsansoni97@gmail.com>
2023-07-24 17:03:44 +0200

    compile on windows. BUG: cannot get queue families

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,11 @@ int main(int argc, char *argv[]) {
     engine.run();
     return 0;
 }
+
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                   _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+    return main(__argc, __argv);
+}
+#endif
